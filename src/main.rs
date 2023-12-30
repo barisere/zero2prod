ac7993615599b81e0515fdf27cd7a4df091d60dd
Barisere Jonathan <20553476+barisere@users.noreply.github.com>
2023-12-30 04:55:39 +0100

    feat: set up basic logging using env_logger

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::{configuration::get_configuration, startup::run};
+use env_logger::Env;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_string = configuration.database.connection_string();
     let connection = PgPool::connect(&connection_string)
         .await
         .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
-    let listener = TcpListener::bind(address).expect("Failed to bind random port");
+    let listener = TcpListener::bind(address).expect("Failed to bind application port");
     run(listener, connection)?.await
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,18 +1,22 @@
-use actix_web::{dev::Server, web, App, HttpServer};
+use actix_web::{dev::Server, web, App, HttpServer, middleware::Logger};
 use sqlx::PgPool;
 use std::net::TcpListener;
+use super::routes::*;
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
-    use super::routes::*;
+    let local_addr = listener.local_addr()?.to_string();
     let connection = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(connection.clone())
     })
     .listen(listener)?
     .run();
+
+    log::info!("Server running on {}", local_addr);
 
     Ok(server)
 }
