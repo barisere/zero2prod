46b948e6a6e0b3c760a49cf6d942d68d79a8d328
Barisere Jonathan <20553476+barisere@users.noreply.github.com>
2024-02-25 16:49:15 +0100

    reject invalid subscribers — validation

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,15 +1,27 @@
+use crate::domain::{NewSubscriber, SubscriberEmail, SubscriberName};
 use actix_web::{
     web::{Data, Form},
     HttpResponse, Responder,
 };
 use chrono::Utc;
 use sqlx::PgPool;
+use unicode_segmentation::UnicodeSegmentation;
 use uuid::Uuid;
 
 #[derive(Debug, serde::Deserialize, serde::Serialize)]
 pub struct SubscribeRequest {
     email: String,
     name: String,
+}
+
+impl TryFrom<SubscribeRequest> for NewSubscriber {
+    type Error = String;
+
+    fn try_from(value: SubscribeRequest) -> Result<Self, Self::Error> {
+        let name = SubscriberName::parse(value.name)?;
+        let email = SubscriberEmail::parse(value.email)?;
+        Ok(Self { email, name })
+    }
 }
 
 #[tracing::instrument(
@@ -21,21 +33,26 @@ pub struct SubscribeRequest {
     )
 )]
 pub async fn subscribe(form: Form<SubscribeRequest>, pool: Data<PgPool>) -> impl Responder {
-    insert_subscriber(&pool, &form)
-        .await
-        .map_or_else(
-            |_| HttpResponse::InternalServerError().finish(),
-            |_| HttpResponse::Ok().finish(),
-        )
+    let new_subscriber = match form.0.try_into() {
+        Ok(name) => name,
+        Err(_) => return HttpResponse::BadRequest().finish(),
+    };
+    insert_subscriber(&pool, &new_subscriber).await.map_or_else(
+        |_| HttpResponse::InternalServerError().finish(),
+        |_| HttpResponse::Ok().finish(),
+    )
 }
 
 #[tracing::instrument(name = "Saving new subscriber details in the database", skip_all)]
-pub async fn insert_subscriber(pool: &PgPool, form: &SubscribeRequest) -> Result<(), sqlx::Error> {
+pub async fn insert_subscriber(
+    pool: &PgPool,
+    new_subscriber: &NewSubscriber,
+) -> Result<(), sqlx::Error> {
     sqlx::query!(
         r#"INSERT INTO subscriptions (id, email, name, subscribed_at) VALUES ($1, $2, $3, $4)"#,
         Uuid::new_v4(),
-        form.email,
-        form.name,
+        new_subscriber.email.as_ref(),
+        new_subscriber.name.as_ref(),
         Utc::now()
     )
     .execute(pool)
@@ -45,4 +62,27 @@ pub async fn insert_subscriber(pool: &PgPool, form: &SubscribeRequest) -> Result
         e
     })?;
     Ok(())
+}
+
+/// Returns `true` if the input satisfies all our validation constraints
+/// on subscriber names, `false` otherwise.
+pub fn is_valid_name(s: &str) -> bool {
+    // `.trim()` returns a view over the input `s` without trailing
+    // whitespace-like characters.
+    // `.is_empty` checks if the view contains any character.
+    let is_empty_or_whitespace = s.trim().is_empty();
+    // A grapheme is defined by the Unicode standard as a "user-perceived"
+    // character: `å` is a single grapheme, but it is composed of two characters
+    // (`a` and `̊`).
+    //
+    // `graphemes` returns an iterator over the graphemes in the input `s`.
+    // `true` specifies that we want to use the extended grapheme definition set,
+    // the recommended one.
+    let is_too_long = s.graphemes(true).count() > 256;
+    // Iterate over all characters in the input `s` to check if any of them matches
+    // one of the characters in the forbidden array.
+    let forbidden_characters = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
+    let contains_forbidden_characters = s.chars().any(|g| forbidden_characters.contains(&g));
+    // Return `false` if any of our conditions have been violated
+    !(is_empty_or_whitespace || is_too_long || contains_forbidden_characters)
 }
