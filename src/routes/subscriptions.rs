d4b2315691cd285eec22864bd685803702f734a8
Barisere Jonathan <20553476+barisere@users.noreply.github.com>
2024-03-25 08:28:25 +0100

    test: verify that email link is sent to unconfirmed subscriber

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -47,15 +47,7 @@ pub async fn subscribe(
     if let Err(_) = insert_subscriber(&pool, &new_subscriber).await {
         return HttpResponse::InternalServerError().finish();
     }
-    if let Err(_) = email_client
-        .send_email(
-            new_subscriber.email,
-            "Welcome!",
-            "Welcome to our newsletter!",
-            "Welcome to our newsletter!",
-        )
-        .await
-    {
+    if let Err(_) = send_confirmation_email(&email_client, new_subscriber).await {
         return HttpResponse::InternalServerError().finish();
     }
     HttpResponse::Ok().finish()
@@ -67,7 +59,7 @@ pub async fn insert_subscriber(
     new_subscriber: &NewSubscriber,
 ) -> Result<(), sqlx::Error> {
     sqlx::query!(
-        r#"INSERT INTO subscriptions (id, email, name, subscribed_at, status) VALUES ($1, $2, $3, $4, 'confirmed')"#,
+        r#"INSERT INTO subscriptions (id, email, name, subscribed_at, status) VALUES ($1, $2, $3, $4, 'pending_confirmation')"#,
         Uuid::new_v4(),
         new_subscriber.email.as_ref(),
         new_subscriber.name.as_ref(),
@@ -103,4 +95,27 @@ pub fn is_valid_name(s: &str) -> bool {
     let contains_forbidden_characters = s.chars().any(|g| forbidden_characters.contains(&g));
     // Return `false` if any of our conditions have been violated
     !(is_empty_or_whitespace || is_too_long || contains_forbidden_characters)
+}
+
+#[tracing::instrument(
+    name = "Send a confirmation email to a new subscriber",
+    skip(email_client, new_subscriber)
+)]
+pub async fn send_confirmation_email(
+    email_client: &EmailClient,
+    new_subscriber: NewSubscriber,
+) -> Result<(), reqwest::Error> {
+    let confirmation_link = "https://my-api.com/subscriptions/confirm";
+    let plain_body = format!(
+        "Welcome to our newsletter!\nVisit {} to confirm your subscription.",
+        confirmation_link
+    );
+    let html_body = format!(
+        "Welcome to our newsletter!<br />\
+         Click <a href=\"{}\">here</a> to confirm your subscription.",
+        confirmation_link
+    );
+    email_client
+        .send_email(new_subscriber.email, "Welcome!", &html_body, &plain_body)
+        .await
 }
